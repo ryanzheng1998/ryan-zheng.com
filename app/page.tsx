ad7342854eb57c9eab34a3acace9b066d2b4ee64
Ryan <ryanzheng1998@gmail.com>
2025-04-15 08:12:43 +0800

    temp

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,15 @@ export default function Home() {
   return (
     <div className="grid place-items-center">
       <Image src="/me.jpeg" width={768 / 3} height={1024 / 3} alt="me" />
-      <p>I'm a software engineer.</p>
+      <p>
+        My name is Ryan. I'm a software engineer. My favorite technologies right
+        now are: React.js, Typescript, Node.js, and PostgreSQL.
+      </p>
+      <br />
+      <h3>Projects</h3>
+      <ul>
+        <li>M</li>
+      </ul>
     </div>
   )
 }
