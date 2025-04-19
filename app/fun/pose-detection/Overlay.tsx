9c2a355a19287d41eaa49923d64d45921aa4131a
Ryan <ryanzheng1998@gmail.com>
2025-04-19 09:06:03 +0800

    clean up

---
+++ app/fun/pose-detection/Overlay.tsx
@@ -0,0 +1,32 @@
+import { useStore } from './useStore'
+
+export const Overlay = () => {
+  const state = useStore()
+  const xScale = state.width / state.videoWidth
+  const yScale = state.height / state.videoHeight
+  const scale = Math.min(xScale, yScale)
+
+  const xOffset = (state.width - state.videoWidth * scale) / 2
+  const yOffset = (state.height - state.videoHeight * scale) / 2
+
+  return (
+    <>
+      <PoseOverlay
+        landmarks={state.poseLandmarkerResult?.landmarks ?? []}
+        width={state.videoWidth}
+        height={state.videoHeight}
+        scale={scale}
+        xOffset={xOffset}
+        yOffset={yOffset}
+      />
+      <HandOverlay
+        landmarks={state.handLandmarkerResult?.landmarks ?? []}
+        width={state.videoWidth}
+        height={state.videoHeight}
+        scale={scale}
+        xOffset={xOffset}
+        yOffset={yOffset}
+      />
+    </>
+  )
+}
--- app/fun/pose-detection/page.tsx
+++ app/fun/pose-detection/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 import { useEffect } from 'react'
-import { HandOverlay } from './HandOverlay'
 import { loadModels } from './loadModels'
 import { onTimeUpdate } from './onTimeUpdate'
-import { PoseOverlay } from './PoseOverlay'
+import { Overlay } from './Overlay'
 import { setupWebcam } from './setupWebcam'
 import { get, set, useStore } from './useStore'
 
@@ -28,13 +27,6 @@ export default function Page() {
     }
   }, [])
 
-  const xScale = state.width / state.videoWidth
-  const yScale = state.height / state.videoHeight
-  const scale = Math.min(xScale, yScale)
-
-  const xOffset = (state.width - state.videoWidth * scale) / 2
-  const yOffset = (state.height - state.videoHeight * scale) / 2
-
   return (
     <div className="relative h-screen w-screen">
       <div
@@ -49,24 +41,7 @@ export default function Page() {
           autoPlay
           className="absolute h-full w-full bg-black"
         />
-        <div className="relative h-full w-full overflow-hidden">
-          <PoseOverlay
-            landmarks={state.poseLandmarkerResult?.landmarks ?? []}
-            width={state.videoWidth}
-            height={state.videoHeight}
-            scale={scale}
-            xOffset={xOffset}
-            yOffset={yOffset}
-          />
-          <HandOverlay
-            landmarks={state.handLandmarkerResult?.landmarks ?? []}
-            width={state.videoWidth}
-            height={state.videoHeight}
-            scale={scale}
-            xOffset={xOffset}
-            yOffset={yOffset}
-          />
-        </div>
+        <Overlay />
       </div>
     </div>
   )
