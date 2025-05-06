eae592090731b35391e380c85d74277bdb162eb0
Ryan <ryanzheng1998@gmail.com>
2025-05-06 22:14:00 +0800

    add google oauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,16 @@
 import NextAuth from 'next-auth'
 import Github from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
 
 const handler = NextAuth({
   providers: [
     Github({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
 })
