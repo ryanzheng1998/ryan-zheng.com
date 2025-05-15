e2650bfa07ba3a4e0315447f9da9b0669a5588d9
Ryan <ryanzheng1998@gmail.com>
2025-05-15 22:00:00 +0800

    change personal picture

--- app/[lang]/fun/ai/document/actions/init.ts
+++ app/[lang]/fun/ai/document/actions/init.ts
@@ -1,7 +1,19 @@
-import { initModel } from './initModel'
-
-const modelId = 'HuggingFaceTB/SmolVLM-500M-Instruct'
+import { pipeline } from '@huggingface/transformers'
 
 export const init = async () => {
-  await initModel()
+  const pipe = await pipeline(
+    'document-question-answering',
+    'Xenova/donut-base-finetuned-docvqa',
+    {
+      device: 'webgpu',
+    },
+  )
+
+  const question = 'what is the longest side of the triangle?'
+
+  const image = await fetch('/test.webp')
+  const imageBlob = await image.blob()
+  const result = await pipe(imageBlob, question)
+
+  console.log('result', result)
 }
