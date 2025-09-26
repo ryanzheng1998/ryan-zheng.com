25759478133e0d1a6fd2a5d6e1c3bbc769047041
Ryan <ryanzheng1998@gmail.com>
2025-09-26 11:02:24 +0800

    remove svg project

--- app/fun/svg/clipper/simplifyPathsD.ts
+++
@@ -1,8 +0,0 @@
-import { PathsD } from 'clipper2-wasm/dist/clipper2z'
-import { getClipper } from './getClipper'
-
-export const simplifyPathsD =
-  (clipper: Awaited<ReturnType<typeof getClipper>>) =>
-  (path: PathsD, epsilon: number = 1) => {
-    return clipper.main.SimplifyPathsD(path, epsilon, true)
-  }
--- app/fun/svg/clipper/union.ts
+++
@@ -1,8 +0,0 @@
-import { PathsD } from 'clipper2-wasm/dist/clipper2z'
-import { getClipper } from './getClipper'
-
-export const union =
-  (clipper: Awaited<ReturnType<typeof getClipper>>) =>
-  (subject: PathsD, clip: PathsD) => {
-    return clipper.main.UnionD(subject, clip, clipper.main.FillRule.NonZero, 8)
-  }
