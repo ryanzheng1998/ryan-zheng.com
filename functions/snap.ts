f5b34a024e42edba029084a20e2ba86912870857
Ryan <ryanzheng1998@gmail.com>
2025-10-30 17:38:00 +0800

    fix image capture

--- functions/snap.ts
+++ functions/snap.ts
@@ -1,6 +1,8 @@
 export const snap = async (stream: MediaStream) => {
   const track = stream.getVideoTracks()[0]
   if (track === undefined) return new Error('No video track found')
+
+  // @ts-ignore
   const imageCapture = new ImageCapture(track)
 
   const blob = await imageCapture.takePhoto()
--- next.config.ts
+++ next.config.ts
@@ -7,21 +7,21 @@ const withMDX = createMDX({
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-
   webpack: (config) => {
-    // --- GLSL loader ---
     config.module.rules.push({
       test: /\.glsl$/,
       type: 'asset/source',
     })
 
-    // --- SVG inline ---
+    config.module.generator['asset/resource'] = config.module.generator['asset']
+    config.module.generator['asset/source'] = config.module.generator['asset']
+    delete config.module.generator['asset']
+
     config.module.rules.push({
       test: /\.svg$/,
       type: 'asset/inline',
     })
 
-    // --- Polyfill Node modules ---
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
@@ -31,7 +31,6 @@ const nextConfig: NextConfig = {
 
     return config
   },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
